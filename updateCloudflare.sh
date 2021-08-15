9fe9704243d70874ab5d24e9cdaf4743da6d5654
Ricardo Andrade <ricardo@feudo.org>
2021-08-14 22:57:15 -0300

    Create updateCloudflare.sh

---
+++ updateCloudflare.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Configuration
+_EMAIL=user@email.com
+_API_TOKEN=XXXXXXXXXXXXX
+_NEW_IP=100.100.100.100
+# Complete the array below with all domain|subdomain entries you want to update at Cloudflare
+_DOMAIN_SUBDOMAIN=(
+    'domain.tld|subdomain.domain.tld'
+)
+
+for _domsub in ${_DOMAIN_SUBDOMAIN[*]};
+    do
+        IFS='|' read -r -a _ds <<< "$_domsub"
+
+       { #iniciar silenciamento de outputs no terminal
+        _zone_id=$(curl -X GET "https://api.cloudflare.com/client/v4/zones?name=${_ds[0]}" \
+            -H "X-Auth-Email: $_EMAIL" \
+            -H "X-Auth-Key: $_API_TOKEN" \
+            -H "Content-Type: application/json" | jq -r '.result[].id')
+
+        _id_dns_record=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$_zone_id/dns_records?type=A&name=${_ds[1]}" \
+                -H "X-Auth-Email: $_EMAIL" \
+                -H "X-Auth-Key: $_API_TOKEN" \
+                -H "Content-Type: application/json" | jq -r '.result[].id')
+
+        curl -X PUT "https://api.cloudflare.com/client/v4/zones/$_zone_id/dns_records/$_id_dns_record" \
+                -H "X-Auth-Email: $_EMAIL" \
+                -H "X-Auth-Key: $_API_TOKEN" \
+                -H "Content-Type: application/json" \
+                --data '{"type":"A","name":"'"${_ds[1]}"'","content":"'"$_NEW_IP"'","ttl":1,"proxied":true}'
+        } &> /dev/null #finalizar silenciamento
+        echo "Subdominio atualizado: ${_ds[1]}"
+    done;
