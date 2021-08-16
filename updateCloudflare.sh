20a72c984214e3da420f84e056abb5fd09567050
Ricardo Andrade <ricardo@feudo.org>
2021-08-16 10:09:05 -0300

    Update updateCloudflare.sh

--- updateCloudflare.sh
+++ updateCloudflare.sh
@@ -15,22 +15,23 @@ for _domsub in ${_DOMAIN_SUBDOMAIN[*]};
     do
         IFS='|' read -r -a _ds <<< "$_domsub"
 
-       { #iniciar silenciamento de outputs no terminal
-        _zone_id=$(curl -X GET "https://api.cloudflare.com/client/v4/zones?name=${_ds[0]}" \
-            -H "X-Auth-Email: $_EMAIL" \
-            -H "X-Auth-Key: $_API_TOKEN" \
-            -H "Content-Type: application/json" | jq -r '.result[].id')
-
-        _id_dns_record=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$_zone_id/dns_records?type=A&name=${_ds[1]}" \
+        { #iniciar silenciamento de outputs no terminal
+            _zone_id=$(curl -X GET "https://api.cloudflare.com/client/v4/zones?name=${_ds[0]}" \
                 -H "X-Auth-Email: $_EMAIL" \
                 -H "X-Auth-Key: $_API_TOKEN" \
                 -H "Content-Type: application/json" | jq -r '.result[].id')
 
-        curl -X PUT "https://api.cloudflare.com/client/v4/zones/$_zone_id/dns_records/$_id_dns_record" \
-                -H "X-Auth-Email: $_EMAIL" \
-                -H "X-Auth-Key: $_API_TOKEN" \
-                -H "Content-Type: application/json" \
-                --data '{"type":"A","name":"'"${_ds[1]}"'","content":"'"$_NEW_IP"'","ttl":1,"proxied":true}'
+            _id_dns_record=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$_zone_id/dns_records?type=A&name=${_ds[1]}" \
+                    -H "X-Auth-Email: $_EMAIL" \
+                    -H "X-Auth-Key: $_API_TOKEN" \
+                    -H "Content-Type: application/json" | jq -r '.result[].id')
+
+            curl -X PUT "https://api.cloudflare.com/client/v4/zones/$_zone_id/dns_records/$_id_dns_record" \
+                    -H "X-Auth-Email: $_EMAIL" \
+                    -H "X-Auth-Key: $_API_TOKEN" \
+                    -H "Content-Type: application/json" \
+                    --data '{"type":"A","name":"'"${_ds[1]}"'","content":"'"$_NEW_IP"'","ttl":1,"proxied":true}'
         } &> /dev/null #finalizar silenciamento
+        
         echo "Subdominio atualizado: ${_ds[1]}"
     done;
