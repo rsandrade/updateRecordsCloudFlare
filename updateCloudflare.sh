104e2bbfc8357aec1ff80d294c2549cc099dbde6
Ricardo Andrade <ricardo@feudo.org>
2021-08-17 02:11:44 -0300

    Update updateCloudflare.sh

--- updateCloudflare.sh
+++ updateCloudflare.sh
@@ -11,26 +11,32 @@ _DOMAIN_SUBDOMAIN=(
 # Auto detect wan address
 _NEW_IP=$(curl api.ipify.org)
 
+# Loop to change IP for every DNS record in _DOMAIN_SUBDOMAIN array
 for _domsub in ${_DOMAIN_SUBDOMAIN[*]};
     do
         IFS='|' read -r -a _ds <<< "$_domsub"
 
         { #iniciar silenciamento de outputs no terminal
+            
+            # Get zone ID
             _zone_id=$(curl -X GET "https://api.cloudflare.com/client/v4/zones?name=${_ds[0]}" \
                 -H "X-Auth-Email: $_EMAIL" \
                 -H "X-Auth-Key: $_API_TOKEN" \
                 -H "Content-Type: application/json" | jq -r '.result[].id')
-
+            
+            # Get DNS record ID
             _id_dns_record=$(curl -X GET "https://api.cloudflare.com/client/v4/zones/$_zone_id/dns_records?type=A&name=${_ds[1]}" \
                 -H "X-Auth-Email: $_EMAIL" \
                 -H "X-Auth-Key: $_API_TOKEN" \
                 -H "Content-Type: application/json" | jq -r '.result[].id')
-
+            
+            # Changes DNS record IP 
             curl -X PUT "https://api.cloudflare.com/client/v4/zones/$_zone_id/dns_records/$_id_dns_record" \
                 -H "X-Auth-Email: $_EMAIL" \
                 -H "X-Auth-Key: $_API_TOKEN" \
                 -H "Content-Type: application/json" \
                 --data '{"type":"A","name":"'"${_ds[1]}"'","content":"'"$_NEW_IP"'","ttl":1,"proxied":true}'
+        
         } &> /dev/null #finalizar silenciamento
         
         echo "Subdominio atualizado: ${_ds[1]}"
